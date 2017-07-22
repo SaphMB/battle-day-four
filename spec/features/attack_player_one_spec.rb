6e0c6199e24ad03567dbbc4cf94865a888aaf51a
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-22 17:00:18 +0100

    edit confirm_hit output

--- spec/features/attack_player_one_spec.rb
+++ spec/features/attack_player_one_spec.rb
@@ -2,6 +2,6 @@ feature 'switching turns' do
   scenario "player two's turn is next" do
     sign_in_and_play
     click_button "Attack"
-    expect(page).to have_content "Ouch! Kat landed a blow!"
+    expect(page).to have_content "Ouch! Kat recieved a blow!"
   end
 end
